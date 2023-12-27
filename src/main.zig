8f1491de21a7d004b3a96ace4768a0083e46506f
set0xc3 <set0xc3@gmail.com>
2023-12-27 13:12:31 +0300

    backup

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,3 @@
-// UI
-// TODO: состояние мыши?
-// TODO: массив объектов.
-// TODO: рисовать объект.
-//          - position
-//          - size
-//          - color
-// TODO: id объекта.
-// TODO: текущий объект.
-// TODO: горячий объект.
-// TODO: фиксировать нажатие по объекту
-
 const std = @import("std");
 
 const r = @cImport({
@@ -20,6 +8,59 @@ const Rect = struct {
     position: @Vector(2, f32),
     size: @Vector(2, f32),
 };
+
+const UI_OBJECTS_SIZE = 256 * 1024;
+
+const UI = struct {
+    const Object = struct {
+        id: []const u8,
+        position: @Vector(2, f32),
+        size: @Vector(2, f32) = .{ 100, 100 },
+    };
+
+    objects_stack: struct {
+        idx: u32,
+        items: [UI_OBJECTS_SIZE]Object,
+    },
+
+    hot_object: ?*Object,
+
+    mouse: struct {
+        position: @Vector(2, f32),
+    },
+
+    frame: u32,
+
+    pub fn init() void {}
+
+    pub fn deInit() void {}
+
+    pub fn beginFrame(self: *@This()) void {
+        if (self.objects_stack.idx >= UI_OBJECTS_SIZE) {
+            self.objects_stack.idx = 0;
+        }
+    }
+
+    pub fn endFrame(self: *@This()) void {
+        self.objects_stack.idx = 0;
+    }
+
+    pub fn drawTestObject(self: *@This(), id: []const u8) void {
+        // var buf: [1024]u8 = undefined;
+        // const newID = std.fmt.bufPrintZ(&buf, "{d}:{s}\n", .{ self.object_idx, id }) catch @panic("error");
+        // std.debug.print("{s}", .{newID});
+
+        self.objects_stack.items[self.objects_stack.idx].id = id;
+        self.objects_stack.items[self.objects_stack.idx].size = .{ 100, 100 };
+        self.objects_stack.idx += 1;
+    }
+};
+
+const WINDOW_WIDTH: i32 = 1280;
+const WINDOW_HEIGHT: i32 = 720;
+const WINDOW_TITLE = "raylib [core] example - basic window";
+
+var ui: UI = undefined;
 
 pub fn is_aabb(a: Rect, b: Rect) bool {
     return a.position[0] + a.size[0] >= b.position[0] and b.position[0] + b.size[0] >= a.position[0] and
@@ -33,122 +74,86 @@ pub fn is_point_aabb(a: @Vector(2, f32), b: Rect) bool {
         a[1] <= b.position[1] + b.size[1];
 }
 
-const UI = struct {
-    const Object = struct {
-        id: []const u8,
-        position: @Vector(2, f32),
-        size: @Vector(2, f32) = .{ 1, 1 },
-    };
-
-    objects: [1000]Object,
-    object_idx: u32,
-
-    hot_object: ?*Object,
-
-    mouse: struct {
-        position: @Vector(2, f32),
-    },
-
-    pub fn init() void {}
-
-    pub fn deInit() void {}
-
-    pub fn beginFrame(self: *@This()) void {
-        if (self.object_idx >= 1000) {
-            self.object_idx = 0;
-        }
-    }
-
-    pub fn endFrame(self: *@This()) void {
-        self.object_idx = 0;
-    }
-
-    pub fn drawTestObject(self: *@This(), id: []const u8, position: @Vector(2, f32), size: @Vector(2, f32)) void {
-        // var buf: [1024]u8 = undefined;
-        // const newID = std.fmt.bufPrintZ(&buf, "{d}:{s}\n", .{ self.object_idx, id }) catch @panic("error");
-        // std.debug.print("{s}", .{newID});
-
-        self.objects[self.object_idx].id = id;
-        self.objects[self.object_idx].position = position;
-        self.objects[self.object_idx].size = size;
-        self.object_idx += 1;
-    }
-};
-
-const WINDOW_WIDTH: i32 = 1280;
-const WINDOW_HEIGHT: i32 = 720;
-const WINDOW_TITLE = "raylib [core] example - basic window";
-
-var ui: UI = undefined;
-
 pub fn main() void {
     r.SetConfigFlags(r.FLAG_VSYNC_HINT | r.FLAG_WINDOW_RESIZABLE | r.FLAG_MSAA_4X_HINT | r.FLAG_WINDOW_HIGHDPI);
     r.InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
     r.SetTargetFPS(60);
 
+    const testObject = Rect{
+        .position = .{ 0, 0 },
+        .size = .{ 100, 100 },
+    };
+    _ = testObject;
+    var offset: @Vector(2, f32) = undefined;
+
     while (!r.WindowShouldClose()) {
-        const screen_w = r.GetScreenWidth();
-        const screen_h = r.GetScreenHeight();
+        const screenW = r.GetScreenWidth();
+        const screenH = r.GetScreenHeight();
 
         var camera: r.Camera2D = undefined;
         // camera.offset = .{ .x = @as(f32, @floatFromInt(screen_w)) / 2.0, .y = @as(f32, @floatFromInt(screen_h)) / 2.0 };
         camera.rotation = 0.0;
         camera.zoom = 1.0;
 
-        const offset: @Vector(2, f32) = undefined;
-        _ = offset;
-        const testObjectPosition = @Vector(2, f32){ 400, 300 };
-        const testObjectSize = @Vector(2, f32){ 100, 100 };
-
         r.ClearBackground(r.RAYWHITE);
         r.BeginDrawing();
         {
             r.BeginMode2D(camera);
             {
-                var mouse_position: @Vector(2, f32) = undefined;
-                mouse_position[0] = r.GetMousePosition().x;
-                mouse_position[1] = r.GetMousePosition().y;
+                const mousePosition = @Vector(2, f32){
+                    r.GetMousePosition().x,
+                    r.GetMousePosition().y,
+                };
+                const mouseDelta = @Vector(2, f32){
+                    r.GetMouseDelta().x,
+                    r.GetMouseDelta().y,
+                };
+                _ = mouseDelta;
 
                 // X
                 {
-                    var point_begin: r.Vector2 = undefined;
-                    point_begin.y = @as(f32, @floatFromInt(screen_h)) / 2;
-                    var point_end: r.Vector2 = undefined;
-                    point_end.x = @as(f32, @floatFromInt(screen_w));
-                    point_end.y = @as(f32, @floatFromInt(screen_h)) / 2;
-                    r.DrawLineV(point_begin, point_end, r.RED);
+                    var pointBegin: r.Vector2 = undefined;
+                    pointBegin.y = @as(f32, @floatFromInt(screenH)) / 2;
+                    var pointEnd: r.Vector2 = undefined;
+                    pointEnd.x = @as(f32, @floatFromInt(screenW));
+                    pointEnd.y = @as(f32, @floatFromInt(screenH)) / 2;
+                    r.DrawLineV(pointBegin, pointEnd, r.RED);
                 }
 
                 // Y
                 {
-                    var point_begin: r.Vector2 = undefined;
-                    point_begin.x = @as(f32, @floatFromInt(screen_w)) / 2;
-                    var point_end: r.Vector2 = undefined;
-                    point_end.x = @as(f32, @floatFromInt(screen_w)) / 2;
-                    point_end.y = @as(f32, @floatFromInt(screen_h));
-                    r.DrawLineV(point_begin, point_end, r.GREEN);
+                    var pointBegin: r.Vector2 = undefined;
+                    pointBegin.x = @as(f32, @floatFromInt(screenW)) / 2;
+                    var pointEnd: r.Vector2 = undefined;
+                    pointEnd.x = @as(f32, @floatFromInt(screenW)) / 2;
+                    pointEnd.y = @as(f32, @floatFromInt(screenH));
+                    r.DrawLineV(pointBegin, pointEnd, r.GREEN);
                 }
 
                 ui.beginFrame();
-                ui.drawTestObject("Object 1", testObjectPosition, testObjectSize);
+
+                ui.drawTestObject("Object 1");
 
                 if (ui.hot_object) |*object| {
                     var buf: [1024]u8 = undefined;
                     _ = std.fmt.bufPrintZ(&buf, "object: {any}\n", .{object}) catch @panic("error");
-                    r.DrawText(&buf[0], 10, 10, 10, r.BLACK);
-
-                    if (r.IsMouseButtonDown(r.MOUSE_BUTTON_LEFT)) {}
+                    object.*.position[0] = mousePosition[0] - offset[0];
+                    object.*.position[1] = mousePosition[1] - offset[1];
 
                     if (r.IsMouseButtonReleased(r.MOUSE_BUTTON_LEFT)) {
                         ui.hot_object = null;
                     }
+
+                    r.DrawText(&buf[0], 10, 10, 10, r.BLACK);
                 }
 
-                for (ui.objects[0..ui.object_idx]) |*object| {
+                for (ui.objects_stack.items[0..ui.objects_stack.idx]) |*object| {
                     const b = Rect{ .position = object.position, .size = object.size };
-                    if (is_point_aabb(mouse_position, b)) {
+                    if (is_point_aabb(mousePosition, b)) {
                         if (r.IsMouseButtonPressed(r.MOUSE_BUTTON_LEFT)) {
                             ui.hot_object = object;
+                            offset[0] = mousePosition[0] - object.position[0];
+                            offset[1] = mousePosition[1] - object.position[1];
                         }
                     }
 
